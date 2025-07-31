da4469ddc20e7a433824278f1311fcc4d6c4cc81
Mohammadali Mirzaei <93537795+ma-mirzaei@users.noreply.github.com>
2025-07-31 11:29:55 +0200

    some refactoring

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Vitruvius Modeler title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/vitruvius modeler/i);
+  expect(titleElement).toBeInTheDocument();
 });
